2578834f2d052dae6534f2736440958373a85e97
emhgit <eharper0815@gmail.com>
2026-04-12 20:59:32 -0500

    update styling and add am id project to projects

--- app/projects/projects.ts
+++ app/projects/projects.ts
@@ -1,78 +1,87 @@
 interface Project {
-    id: string;
-    name: string;
-    description: string;
-    githubUrl?: string;
-    liveUrl?: string;
-    imageUrl?: string;
-    tags?: string[];
+  id: string;
+  name: string;
+  description: string;
+  githubUrl?: string;
+  liveUrl?: string;
+  imageUrl?: string;
+  tags?: string[];
 }
 
 export const projects: Project[] = [
-    {
-        id: "1",
-        name: "USACO Guide (Next.js Migration)",
-        tags: [
-            "Next.js",
-            "Gatsby",
-            "Algolia",
-            "MDX",
-            "TypeScript",
-            "Vercel",
-            "Firebase",
-            "SQLite",
-        ],
-        description:
-            "Migrated the USACO Guide website from Gatsby to Next.js to improve performance and developer experience.",
-        githubUrl:
-            "https://github.com/cpinitiative/usaco-guide/blob/master/docs/MIGRATION.md",
-        liveUrl: "https://usaco.guide",
-        imageUrl: "/usaco-guide-banner-image-big.jpg",
-    },
-    {
-        id: "Economic Data Visualizer",
-        name: "Economic Data Visualizer",
-        tags: ["Vanilla JavaScript", "Chart.js", "World Bank API", "Cloudflare"],
-        description:
-            "Fetches real-time economic indicators from the World Bank API and allows users to generate visual dashboards using Chart.js.",
-        githubUrl: "https://github.com/emhgit/economic-data-visualizer",
-        liveUrl: "https://economic-data-visualizer.net/",
-        imageUrl: "/economic-data-visualizer-thumb.png",
-    },
-    {
-        id: "cp",
-        name: "Competitive Programming",
-        tags: ["C++", "Java", "Maven"],
-        description:
-            "A collection of competitive programming problems and solutions in C++ and Java.",
-        githubUrl: "https://github.com/emhgit/competitive-programming",
-    },
-    {
-        id: "cpinitiative",
-        name: "Competitive Programming Initiative (CPI)",
-        tags: ["Next.js", "Tailwind CSS", "Vercel"],
-        description:
-            "Maintained website for the Competitive Programming Initiative (CPI), a non-profit organization dedicated to promoting competitive programming education and resources.",
-        githubUrl: "https://github.com/cpinitiative/cpinitiative",
-        liveUrl: "https://joincpi.org",
-        imageUrl: "/cpi-ss.png",
-    },
-    {
-        id: "java-lsp",
-        name: "(Coming Soon) USACO Guide IDE Java LSP",
-        tags: ["Python", "FastAPI", "JDTLS", "Modal", "Monaco Editor", "Docker"],
-        description: "An LSP (Language Server Protocol) implementation for Java.",
-        githubUrl: "https://github.com/cpinitiative/ide-lsp-modal",
-        liveUrl: "https://ide.usaco.guide",
-        imageUrl: "/java-lsp-ss.png",
-    },
-    {
-        id: "personal-website",
-        name: "Personal Website (This)",
-        tags: ["Next.js", "Tailwind CSS", "Vercel"],
-        description:
-            "A personal website built with Next.js and Tailwind CSS, deployed with Vercel.",
-        githubUrl: "https://github.com/emhgit/website",
-        liveUrl: "https://elliottmharper.dev",
-    },
+  {
+    id: "1",
+    name: "USACO Guide (Next.js Migration)",
+    tags: [
+      "Next.js",
+      "Gatsby",
+      "Algolia",
+      "MDX",
+      "TypeScript",
+      "Vercel",
+      "Firebase",
+      "SQLite",
+    ],
+    description:
+      "Migrated the USACO Guide website from Gatsby to Next.js to improve performance and developer experience.",
+    githubUrl:
+      "https://github.com/cpinitiative/usaco-guide/blob/master/docs/MIGRATION.md",
+    liveUrl: "https://usaco.guide",
+    imageUrl: "/usaco-guide-banner-image-big.jpg",
+  },
+  {
+    id: "Economic Data Visualizer",
+    name: "Economic Data Visualizer",
+    tags: ["Vanilla JavaScript", "Chart.js", "World Bank API", "Cloudflare"],
+    description:
+      "Fetches real-time economic indicators from the World Bank API and allows users to generate visual dashboards using Chart.js.",
+    githubUrl: "https://github.com/emhgit/economic-data-visualizer",
+    liveUrl: "https://economic-data-visualizer.net/",
+    imageUrl: "/economic-data-visualizer-thumb.png",
+  },
+  {
+    id: "american-identity",
+    name: "American Identity Research Project",
+    tags: ["Next.js", "MDX", "Vercel", "Historical Research"],
+    description:
+      "Historical research project that studies four core American ideals—democracy, equal protection and due process, freedom of expression, and privacy—and their conflict across U.S. History. Displayed using rendered MDX content.",
+    liveUrl: "https://elliottmharper.dev/american-identity",
+    imageUrl: "/am-id-ss.png",
+  },
+  {
+    id: "cp",
+    name: "Competitive Programming",
+    tags: ["C++", "Java", "Maven"],
+    description:
+      "A collection of competitive programming problems and solutions in C++ and Java.",
+    githubUrl: "https://github.com/emhgit/competitive-programming",
+  },
+  {
+    id: "cpinitiative",
+    name: "Competitive Programming Initiative (CPI)",
+    tags: ["Next.js", "Tailwind CSS", "Vercel"],
+    description:
+      "Maintained website for the Competitive Programming Initiative (CPI), a non-profit organization dedicated to promoting competitive programming education and resources.",
+    githubUrl: "https://github.com/cpinitiative/cpinitiative",
+    liveUrl: "https://joincpi.org",
+    imageUrl: "/cpi-ss.png",
+  },
+  {
+    id: "java-lsp",
+    name: "(Coming Soon) USACO Guide IDE Java LSP",
+    tags: ["Python", "FastAPI", "JDTLS", "Modal", "Monaco Editor", "Docker"],
+    description: "An LSP (Language Server Protocol) implementation for Java.",
+    githubUrl: "https://github.com/cpinitiative/ide-lsp-modal",
+    liveUrl: "https://ide.usaco.guide",
+    imageUrl: "/java-lsp-ss.png",
+  },
+  {
+    id: "personal-website",
+    name: "Personal Website (This)",
+    tags: ["Next.js", "Tailwind CSS", "Vercel"],
+    description:
+      "A personal website built with Next.js and Tailwind CSS, deployed with Vercel.",
+    githubUrl: "https://github.com/emhgit/website",
+    liveUrl: "https://elliottmharper.dev",
+  },
 ];
